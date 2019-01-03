16bb604a33b8188917c467e64cb848b00395f0b2
Tara Weinreb <36668765+ataraweinreb@users.noreply.github.com>
2019-01-02 22:08:15 -0500

    Update BaseballGame.java

--- BaseballGame.java
+++ BaseballGame.java
@@ -1,57 +1,53 @@
-class Solution {
-       public static int calPoints(String[] ops) {
-        
-        int sum = 0;
-        int valid1 = -1000;
-        int valid2 = -1000;
-        
-        
-        for (int i = 0; i < ops.length; i++) {
-            if (Character.isDigit(ops[i].charAt(0)) 
-            		|| ops[i].length() > 1 && Character.isDigit(ops[i].charAt(1))) {
-                int temp = Integer.parseInt(ops[i]);
-                sum += temp;
-                
-                if (valid1 == -1000) {
-                    valid1 = temp;
-                }
-                else if (valid2 == -1000){
-                    valid2 = temp;
-                }
-                else { //both not negative
-                    valid1 = valid2;
-                    valid2 = temp;
-                }
-            }
-            else if (ops[i].charAt(0) == '+') {
-                sum += valid1 + valid2;
-                
-                int temp = valid1;
-                valid1 = valid2;
-                valid2 = temp + valid1;
-            }
-            else if (ops[i].charAt(0) == 'D') {
-            	if (valid2 == -1000) {
-            		sum += (valid1 * 2);
-            		valid2 = (valid1 * 2);
-            	}
-            	else {
-            		sum += (valid2 * 2);
-            		valid1 = valid2;
-                    valid2 = valid1 * 2;
-            	}
-            }
-            else { //'C'
-                if (valid1 != -1000 && valid2 == -1000) {
-                    sum -= valid1;
-                    valid1 = -1000;
-                }
-                else {
-                    sum -= valid2;
-                    valid2 = -1000;
-                }
-            }
-        } 
-        return sum;
+/**
+* Given a list of strings, each string can be one of the 4 following types: 
+* Integer (one round's score): Directly represents the number of points you get in this round.
+* "+" (one round's score): Represents that the points you get in this round are the sum of the last two valid round's points.
+* "D" (one round's score): Represents that the points you get in this round are the doubled data of the last valid round's points.
+* "C" (an operation, which isn't a round's score): Represents the last valid round's points you get were invalid and should be removed.
+*
+* You need to return the sum of the points you could get in all the rounds. 
+* 
+* @author Atara Weinreb
+*/
+class BaseballGame {
+       int sum = 0, last = 0, secondToLast = 0, temp = 0;
+          
+           Stack<Integer> s = new Stack<>();
+           
+           for (int i = 0; i < ops.length; i++) {
+               if (ops[i].charAt(0) == '+') {
+                   last = s.pop();
+                   secondToLast = s.pop();
+                   temp = last + secondToLast;
+                   
+                   sum += temp;
+                   
+                   s.push(secondToLast);
+                   s.push(last);
+                   s.push(temp);
+               }
+               else if (ops[i].charAt(0) == 'D') {
+                   last = s.peek();
+                   temp = (last * 2);
+                   
+                   sum += temp;
+                   
+                   s.push(temp);
+               }
+               else if (ops[i].charAt(0) == 'C') {
+                   temp = s.peek();
+                   s.pop();
+                   
+                   sum -= temp;
+               }
+               else {
+                   temp = Integer.parseInt(ops[i]);
+                   
+                   sum += temp;
+                   
+                   s.push(temp);
+               }
+           }
+        return sum;   
     }
 }
